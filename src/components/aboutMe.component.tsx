3a61b66be5daae869d2d731016411f03c365ae44
PickolZi <pickol876@gmail.com>
2024-06-10 13:17:07 -0700

    Added containers and centered them. Added sticky header for aboutMe section as well

--- src/components/aboutMe.component.tsx
+++ src/components/aboutMe.component.tsx
@@ -7,81 +7,36 @@ const AboutMe = () => {
   return (
     <TransitionScreen
       id="aboutMe"
-      imgUrl="https://i.pinimg.com/originals/ec/d6/72/ecd67255b08380eb534d3f8ee397bc85.gif"
+      imgUrl="https://wallpapercave.com/wp/wp2673417.jpg"
     >
-      <section>
-        <div>
-          <h1 className="text-2xl text-center mb-2">About Me</h1>
-          <div>
-            <Image
-              src={"/aboutMe/meProm.jpg"}
-              alt="Picture of me gazing at the stars"
-              width={300}
-              height={300}
-              className="relative mx-auto rounded-md w-[300px] h-[300px] object-cover"
-            />
-
-            <div className="flex flex-col gap-4 max-w-[60ch] mx-auto">
-              <p className="text-lg text-gray-400">Hi, i&apos;m james!</p>
-              <p className="text-lg text-gray-400">
-                I am a fullstack developer mostly self-taught through side
-                projects, books, Udemy courses, and YouTube. I am also currently
-                pursuing for my Bachelor&apos;s degree in Computer Science.
-              </p>
-              <p className="text-lg text-gray-400">
-                In my free time I enjoy creating solutions through code, reading
-                books, exercising, and having fun with friends!
-              </p>
-            </div>
-          </div>
-
-          {/* <div>
-            <Image
-              src={"/aboutMe/books.jpg"}
-              alt="Picture of me gazing at the stars"
-              width={300}
-              height={300}
-              className="relative mx-auto rounded-md w-[300px] h-[300px] object-cover"
-            />
-
-            <div className="flex flex-col gap-4 max-w-[60ch] mx-auto">
-              <p className="text-lg text-gray-400">Hi, i&apos;m james!</p>
-              <p className="text-lg text-gray-400">
-                I am a fullstack developer mostly self-taught through side
-                projects, books, Udemy courses, and YouTube. I am also currently
-                pursuing for my Bachelor&apos;s degree in Computer Science.
-              </p>
-              <p className="text-lg text-gray-400">
-                In my free time I enjoy creating solutions through code, reading
-                books, exercising, and having fun with friends!
-              </p>
-            </div>
-          </div> */}
-
-          {/* <div>
-            <Image
-              src={"/aboutMe/meGym.jpg"}
-              alt="Picture of me gazing at the stars"
-              width={300}
-              height={300}
-              className="relative mx-auto rounded-md w-[300px] h-[300px] object-cover"
-            />
-
-            <div className="flex flex-col gap-4 max-w-[60ch] mx-auto">
-              <p className="text-lg text-gray-400">Hi, i&apos;m james!</p>
-              <p className="text-lg text-gray-400">
-                I am a fullstack developer mostly self-taught through side
-                projects, books, Udemy courses, and YouTube. I am also currently
-                pursuing for my Bachelor&apos;s degree in Computer Science.
-              </p>
-              <p className="text-lg text-gray-400">
-                In my free time I enjoy creating solutions through code, reading
-                books, exercising, and having fun with friends!
-              </p>
-            </div>
-          </div> */}
+      <div className="flex flex-col h-screen relative">
+        <div className="relative h-[300px]">
+          <h1 className="headerText">About Me</h1>
         </div>
-      </section>
+
+        <div className="container flex flex-grow flex-col justify-center items-center">
+          <Image
+            src={"/aboutMe/meProm.jpg"}
+            alt="Picture of James San from his senior prom night."
+            width={256}
+            height={256}
+            className="relative mx-auto rounded-full w-[256px] h-[256px] object-cover"
+          />
+
+          <div className="dim-background flex flex-col gap-4 max-w-[60ch] mx-auto">
+            <p className="text-lg">Hi, i&apos;m james!</p>
+            <p className="text-lg">
+              I am a fullstack developer mostly self-taught through side
+              projects, books, Udemy courses, and YouTube. I am also currently
+              pursuing for my Bachelor&apos;s degree in Computer Science.
+            </p>
+            <p className="text-lg">
+              In my free time I enjoy creating solutions through code, reading
+              books, exercising, and having fun with friends!
+            </p>
+          </div>
+        </div>
+      </div>
     </TransitionScreen>
   );
 };
